31168ee8a4d909a655ee667a9372a3d1fe53bab2
CIS 550 <cis550@seas.upenn.edu>
2016-04-22 07:03:30 +0100

    Added S3 fileupload code

--- DatalakeManagementSystemNode/aws_test.js
+++ DatalakeManagementSystemNode/aws_test.js
@@ -1,8 +1,23 @@
+var express = require('express');
+var app = express();
 var aws = require('aws-sdk');
 var fs = require('fs');
 var bucket = 'dlms_documents';
 var s3 = new aws.S3();
 var http = require('http');
+
+app.get('/', function (req, res) {
+	   res.send('Hello World');
+	});
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
+
+});
 
 function getContentTypeByFile(fileName) {
 	var rc = 'application/octet-stream';
@@ -41,39 +56,3 @@ function uploadFile(remoteFilename, fileName) {
 		console.log(arguments);
 	});
 }
-
-var url = require('url');
-
-http.createServer(function handler(request, response) {
-	// parse the request
-	var pathname = url.parse(request.url).pathname;
-
-	// print the requested pathname to console
-	console.log("File requested:" + pathname);
-
-	if (url === '/uploadfile') {
-		console.log(req);
-	}
-	else {
-		// read the file from the filesystem
-		fs.readFile(pathname.substr(1), function(err, data) {
-			if (err) {
-				console.log(err);
-				response.writeHead(404, {
-					'Content-Type' : 'text/html'
-				});
-			} else {
-				response.writeHead(200, {
-					'Content-Type' : 'text/html'
-				});
-				response.write(data.toString());
-			}
-	}
-
-		// end of response
-		response.end();
-
-	});
-}).listen(8080);
-
-console.log('Server running at http://127.0.0.1:8080/');
--- DatalakeManagementSystemNodeExpress/routes/index.js
+++ DatalakeManagementSystemNodeExpress/routes/index.js
@@ -1,9 +1,88 @@
 var express = require('express');
 var router = express.Router();
+var util = require("util");
+var fs = require("fs");
+var aws = require('aws-sdk');
+var credentials = new aws.SharedIniFileCredentials({profile: 'default'});
+aws.config.credentials = credentials;
+var bucket = 'dlms-documents';
+var s3 = new aws.S3();
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', {
+		title : 'DLMS'
+	});
+});
+
+router.get('/index', function(req, res, next) {
+	res.render('index', {
+		title : 'DLMS'
+	});
+});
+
+router.get('/upload', function(req, res, next) {
+	res.render('upload', {
+		title : 'DLMS'
+	});
+});
+
+
+function getContentTypeByFile(fileName) {
+	var rc = 'application/octet-stream';
+	var fn = fileName.toLowerCase();
+
+	if (fn.indexOf('.html') >= 0) {
+		rc = 'text/html';
+	} else if (fn.indexOf('.css') >= 0) {
+		rc = 'text/css';
+	} else if (fn.indexOf('.json') >= 0) {
+		rc = 'application/json';
+	} else if (fn.indexOf('.js') >= 0) {
+		rc = 'application/x-javascript';
+	} else if (fn.indexOf('.png') >= 0) {
+		rc = 'image/png';
+	} else if (fn.indexOf('.jpg') >= 0) {
+		rc = 'image/jpg';
+	}
+
+	return rc;
+}
+
+function uploadFile(remoteFilename, fileName) {
+	var fileBuffer = fs.readFileSync(fileName);
+	var metaData = getContentTypeByFile(fileName);
+
+	s3.putObject({
+		ACL : 'public-read',
+		Bucket : bucket,
+		Key : remoteFilename,
+		Body : fileBuffer,
+		ContentType : metaData
+	}, function(error, response) {
+		console.log('uploaded file[' + fileName + '] to [' + remoteFilename + '] as [' + metaData + ']');
+		console.log(arguments);
+	});
+}
+
+router.post('/uploadfile', function(req, res, next) {
+	if (req.files) {
+		console.log(util.inspect(req.files));
+		if (req.files.dataitem.size === 0) {
+		            return next(new Error("Hey, first would you select a file?"));
+		}
+		fs.exists(req.files.dataitem.path, function(exists) {
+			if(exists) {
+				console.log("New file uploaded at - %s", req.files.dataitem.path);
+				console.log("Uploading fileto S3");
+				uploadFile("temp", req.files.dataitem.path);
+				res.send("Got your file!");
+			} else {
+				res.send("Well, there is no magic for those who don’t believe in it!");
+			}
+		});
+	}
 });
 
 module.exports = router;

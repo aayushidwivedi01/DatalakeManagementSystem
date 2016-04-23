34ee16e89705b9c479299f8932af7b2aa68bf1f5
aayushidwivedi01 <aayushi20@gmail.com>
2016-04-24 00:30:34 +0100

    Added create account and login verification

--- DatalakeManagementSystemNodeExpress/routes/account.js
+++ DatalakeManagementSystemNodeExpress/routes/account.js
@@ -1,18 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../models/models");
 
 function verify_account(req, res){
-	var username = req.body.username;
-	var password = req.body.password;
-	var passwordconfirm = req.body.passwordconfirm;
-	
-	if (!username || !password || password != passwordconfirm){
-		res.send("Incorrect username or password");
-	}else{
-		//TO-DO: verify account details
-		login_sucess(req, res, username);
+	var name = req.body.username;
+	var passwd = req.body.password;
+	var passwdconfirm = req.body.passwordconfirm;
+	var usr = new User({username: name, password: passwd});
+	if (!name || !passwd || !passwdconfirm){
+		//res.redirect()
+		console.log("Incorrect Username/password");
+		res.send("Username or password incorrect. %s", usr);
+	} else if (passwd != passwdconfirm){
+		console.log("Passwords don't match");
+		res.send("Passwords don't match %s", usr);
+	} else {
+		User.findOne({username: name}, function(err, checkUsr){
+			if(err){
+				console.log("Username already exists. Select a new username.");
+				res.send("Username already exists. Select a new username. %s", checkUsr);
+			}
+			
+			usr.save(function(err){
+			if(err){
+				console.log("Error adding new user");
+				res.send("Username already exists. Select a new username. %s", checkUsr);
+			}
+			console.log("User has been saved!");
+			session = req.session;
+			session.user = name;
+			res.render('homepage',
+					{'title':"Welcome, "+ name + "!"})
+			});
+			
+			
+		});
+		
 	}
+	
 };
+
 
 exports.do_work = 
 	  function(req,res, next) {
--- DatalakeManagementSystemNodeExpress/routes/login.js
+++ DatalakeManagementSystemNodeExpress/routes/login.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require("../models/models");
 
 function verify_account(req, res){
-	var username = req.body.username;
-	var password = req.body.password;
+	var name = req.body.username;
+	var passwd = req.body.password;
 	
-	if (!username || !password){
+	if (!name || !passwd){
 		res.send("Incorrect username or password");
 	}else{
-		//TO-DO: verify account details
-		login_sucess(req, res, username);
+		//verify account details
+		User.findOne({username: name}, function(err, checkUsr){
+			if(err){
+				console.log("User not found");
+				res.send("Username incorrect.");
+			}
+			else{
+				if (checkUsr.password === passwd){
+					login_sucess(req, res, name);
+				}
+				else{
+					console.log("Password incorrect");
+					res.send("Incorrect password");
+				}
+			}
+		});
+		
 	}
 };
 	

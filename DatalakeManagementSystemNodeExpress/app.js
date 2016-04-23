9f0ab37fedfa1478b693fd3d1a2a527b60739e58
CIS 550 <cis550@seas.upenn.edu>
2016-04-23 02:10:06 +0100

    Adding session

--- DatalakeManagementSystemNodeExpress/app.js
+++ DatalakeManagementSystemNodeExpress/app.js
@@ -3,14 +3,19 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 var multiparty = require('connect-multiparty');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
-
+var homepage = require('./routes/homepage');
 var app = express();
 
+app.use(cookieParser());
+app.use(session({secret: 'shh1243',
+				resave:false,
+				saveUninitialized:true}));
 
 app.use(multiparty({}));
 
@@ -21,13 +26,14 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended : false
+	extended : true
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/homepage', homepage);
 app.post('/login', login.do_work);
 
 
--- DatalakeManagementSystemNodeExpress/routes/login.js
+++ DatalakeManagementSystemNodeExpress/routes/login.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+
+function verify_account(req, res){
+	var username = req.body.username;
+	var password = req.body.password;
+	
+	if (!username || !password){
+		res.send("Incorrect username or password");
+	}else{
+		//TO-DO: verify account details
+		login_sucess(req, res, username);
+	}
+};
+	
+function login_sucess(req,res, username){
+	session = req.session;
+	session.user = username;
+	res.render('homepage',
+			{'title':"Welcome, "+ username + "!"})
+};
+	
 exports.do_work = 
-  function(req, res, next) {
-  res.send('need to implement login');
+  function(req,res, next) {
+	verify_account(req, res);
 };
 
 //module.exports = router;

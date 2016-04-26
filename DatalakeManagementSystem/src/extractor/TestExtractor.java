1fe1edc791c704b6facd4d370dda9c549ac398a4
CIS 455/555 Student <anwesha@seas.upenn.edu>
2016-04-26 00:31:19 -0400

    Added XML extractor

--- DatalakeManagementSystem/src/extractor/TestExtractor.java
+++ DatalakeManagementSystem/src/extractor/TestExtractor.java
@@ -1,12 +1,21 @@
 package extractor;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.tika.Tika;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Test extractor. 
@@ -18,30 +27,70 @@ import org.apache.tika.Tika;
 public class TestExtractor {
 
 	public static void main(String[] args) throws IOException {
-		String filename = args[0];
-		Tika tika = new Tika();
-		String mediaType = tika.detect(filename);
-//		System.out.println(mediaType);
+//		String file_arg = "/home/cis455/git/dlms_little_bobby_tables/DatalakeManagementSystem/src/extractor/generated.json";
+		String file_arg = args[0];
 		
-		if(mediaType.equals("application/json")){
-			InputStream is = new FileInputStream(filename);
-	        String jsonTxt = IOUtils.toString(is);
-	        String out = JsonExtract.extractJson(jsonTxt,"generated.json");
-	        ArrayList<String> extracted_pairs = readJsonOutput.getExtractedPairs(out);
-	        
-	        for(String pair : extracted_pairs){
-	        	System.out.println(pair);
-	        }
-		}else if(mediaType.equals("text/csv")){
-			String out = CSVExtract.extractCSV(filename);
-//			System.out.println(out);
-			ArrayList<String> extracted_pairs = readJsonOutput.getExtractedPairs(out);
-	        
-	        for(String pair : extracted_pairs){
-	        	System.out.println(pair);
-	        }
+		//Check if directory or file
+		File file = new File(file_arg);
+		List<String> files = new ArrayList<String>();
+		
+		if(file.isDirectory()){
+			files = Arrays.asList(file.list());
+		}else{
+			files.add(file_arg);
 		}
-
+		
+		Tika tika = new Tika();
+		ArrayList<String> extracted_pairs = new ArrayList<String>();
+		
+		for(String filename: files){
+			String mediaType = tika.detect(filename);
+			System.out.println(mediaType);
+			
+			//PARSE JSON
+			if(mediaType.equals("application/json")){
+				
+//				final InputStream in = new FileInputStream(filename);
+//				try {
+//				  for (Iterator it = new ObjectMapper().readValues(
+//				      new JsonFactory().createJsonParser(in), Map.class); it.hasNext();)
+//				    System.out.println(IOUtils.toString((InputStream) it.next()));
+//				  	
+//				}
+//				finally { in.close();} 
+				
+				InputStream is = new FileInputStream(filename);
+		        String jsonTxt = IOUtils.toString(is);
+		        String out = JsonExtract.extractJson(jsonTxt,filename);
+		        extracted_pairs = readJsonOutput.getExtractedPairs(out);
+		        
+		        for(String pair : extracted_pairs){
+		        	System.out.println(pair);
+		        }
+			}
+			//PARSE CSV
+			else if(mediaType.equals("text/csv")){
+				String out = CSVExtract.extractCSV(filename);
+				extracted_pairs = readJsonOutput.getExtractedPairs(out);
+		        
+		        for(String pair : extracted_pairs){
+		        	System.out.println(pair);
+		        }
+			} 
+			//PARSE XML
+			else if(mediaType.equals("application/xml")){
+				XMLExtract saxparser = new XMLExtract();
+				saxparser.extractXML(filename);
+				extracted_pairs = saxparser.getLeafNodes();
+				
+				for(String pair : extracted_pairs){
+		        	System.out.println(pair);
+		        }
+				
+			}
+		}
+		
+		//CALL INVERTED INDEX METHOD
 
 	}
 

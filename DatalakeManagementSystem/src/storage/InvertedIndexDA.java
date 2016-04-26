df0779f3f2b98c5fe273ae07bc72b987c2063247
mishra14 <mishra14@gmail.com>
2016-04-26 01:54:08 +0100

    fixed update in inverted index DA

--- DatalakeManagementSystem/src/storage/InvertedIndexDA.java
+++ DatalakeManagementSystem/src/storage/InvertedIndexDA.java
@@ -6,6 +6,7 @@ import static com.mongodb.client.model.Updates.set;
 import java.util.ArrayList;
 import java.util.List;
 import org.bson.Document;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
@@ -45,6 +46,7 @@ public class InvertedIndexDA {
 	@SuppressWarnings("unchecked")
 	public InvertedIndex fetch(String username) {
 		Document doc = collection.find(eq(WORD_KEY, username)).first();
+		System.out.println("Fetched doc : " + doc);
 		InvertedIndex iIndex = null;
 		if (doc != null) {
 			List<Occurance> occurs = new ArrayList<Occurance>();
@@ -56,18 +58,22 @@ public class InvertedIndexDA {
 		return iIndex;
 	}
 
-	public void store(InvertedIndex fIndex) {
-		Document doc = Document.parse(new JSONObject(fIndex).toString());
+	public void store(InvertedIndex iIndex) {
+		Document doc = Document.parse(new JSONObject(iIndex).toString());
 		collection.insertOne(doc);
 	}
 
-	public void update(InvertedIndex fIndex) {
-		// collection.updateOne(eq(WORD_KEY, fIndex.getPath()),
-		// set(OCCURS_KEY,fIndex.getValue()));
+	public void update(InvertedIndex iIndex) {
+		/*Document doc = Document.parse(new JSONObject(iIndex).toString());
+		System.out.println(new JSONArray(iIndex.getOccurs()));
+		collection.updateOne(eq(WORD_KEY, iIndex.getWord()),
+				set(OCCURS_KEY, doc.get(OCCURS_KEY)));*/
+		delete(iIndex);
+		store(iIndex);
 	}
 
-	public void delete(InvertedIndex fIndex) {
-		collection.deleteOne(eq(WORD_KEY, fIndex.getWord()));
+	public void delete(InvertedIndex iIndex) {
+		collection.deleteOne(eq(WORD_KEY, iIndex.getWord()));
 	}
 
 	public void delete(String username) {
@@ -82,17 +88,25 @@ public class InvertedIndexDA {
 		ArrayList<Occurance> occurs = new ArrayList<Occurance>();
 		occurs.add(new Occurance("user1_tst.xml/title", "title"));
 		occurs.add(new Occurance("user1_tst2.xml/title", "title"));
-		occurs.add(new Occurance("user1_tst.xml/title", "content"));
+		occurs.add(new Occurance("user1_tst.xml/content", "content"));
 		InvertedIndex iIndex = new InvertedIndex("test_word", occurs);
 		InvertedIndexDA iIndexDA = new InvertedIndexDA();
-		iIndexDA.store(iIndex);
-		System.out.println(iIndexDA.fetch("test_word"));
-		// iIndexDA.update(new InvertedIndex("user1_tst.xml/title", "my doc
-		// 2"));
-		System.out.println(iIndexDA.fetch("test_word"));
-		iIndexDA.delete("test_word");
-		System.out.println(iIndexDA.fetch("test_word"));
-		iIndexDA.close();
+		try {
+			
+			iIndexDA.store(iIndex);
+			System.out.println(iIndexDA.fetch("test_word"));
+			occurs.add(new Occurance("user1_tst2.xml/owner", "owner"));
+			iIndexDA.update(iIndex);
+			System.out.println(iIndexDA.fetch("test_word"));
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			iIndexDA.delete("test_word");
+			System.out.println(iIndexDA.fetch("test_word"));
+			iIndexDA.close();
+		}
 	}
 
 }

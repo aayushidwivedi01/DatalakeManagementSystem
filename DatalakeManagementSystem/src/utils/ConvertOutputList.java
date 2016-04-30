a454db1d0e1aad7b86e227978313e9f6221d9d46
CIS 455/555 Student <anwesha@seas.upenn.edu>
2016-04-30 00:22:43 -0400

    Now displaying proper tree structure

--- DatalakeManagementSystem/src/utils/ConvertOutputList.java
+++ DatalakeManagementSystem/src/utils/ConvertOutputList.java
@@ -17,30 +17,52 @@ public class ConvertOutputList {
 	
 	public void createJson(ArrayList<String> list){
 		
-		JSONArray jarray = new JSONArray();
 		ArrayList<Integer> end_points = new ArrayList<Integer>();
 		
 		int j = 0;
-		
+		int common_idx = 0;
 		for(String entry : list){
 			String [] components = entry.split("/");
-			end_points.add(j+components.length-1);
-			
 			int i;
 			
-			for(i = j; i < j+components.length; i++){
+			//Check if leaf node of this path already has been seen
+			common_idx = 0;
+			JSONObject common = new JSONObject();
+			common.put("name", components[common_idx]);
+			
+			while(nodes.contains(common)){
+				common.clear();
+				
+				if(++common_idx < components.length)
+					common.put("name", components[common_idx]);
+				else
+					break;
+			}
+			
+			for(i = j; i-j+common_idx < components.length; i++){
 				JSONObject name = new JSONObject();
-				name.put("name", components[i-j]);
+				name.put("name", components[i-j+common_idx]);
 				nodes.add(name);
 				
-				if(i+1 != j+components.length){
+				if(i != j+components.length-1-common_idx){
 					JSONObject link = new JSONObject();
-					link.put("source", i);
-					link.put("target", i+1);
+					
+					if(i == j && common_idx > 0){
+						JSONObject common_link = new JSONObject();
+						common_link.put("source", i-common_idx);
+						common_link.put("target", i);
+						links.add(common_link);
+						link.put("source", i);
+						link.put("target", i+1);
+						
+					}else{
+						link.put("source", i);
+						link.put("target", i+1);
+					}
 					links.add(link);
 				}
 			}
-			
+			end_points.add(j+components.length-1-common_idx);
 			j = i;
 		}
 		
@@ -52,15 +74,11 @@ public class ConvertOutputList {
 				links.add(link);
 			}
 		}
-		
+		System.out.println(links.toJSONString());
 		JSONObject final_json = new JSONObject();
 		
-//		String [] nodesArr = new String[nodes.size()];
-//		nodesArr = nodes.toArray(nodesArr);
 		final_json.put("nodes", nodes);
 		
-//		String [] linksArr = new String[links.size()];
-//		linksArr = links.toArray(linksArr);
 		final_json.put("links", links);
 		
 		System.out.println(final_json.toString());
@@ -69,6 +87,7 @@ public class ConvertOutputList {
 	public static void main(String[] args) {
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("DOC1/b/tom");
+		list.add("DOC1/b/e/tom");
 		list.add("DOC2/d/tom");
 		list.add("DOC3/tom");
 		list.add("DOC4/x/brady");

f4b90f7f3bdf43e6ed062c020fc5172f1e2760ef
CIS 455/555 Student <anwesha@seas.upenn.edu>
2016-05-01 00:53:40 -0400

    Revert "fixed bug in displaying tree structure"
    This reverts commit 962308e95ab47f4d209d43830c8b1451f789605f.

--- DatalakeManagementSystem/src/searchengine/GenerateOutputJson.java
+++ DatalakeManagementSystem/src/searchengine/GenerateOutputJson.java
@@ -3,8 +3,6 @@ package searchengine;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -19,52 +17,45 @@ public class GenerateOutputJson {
 		links = new JSONArray();
 	}
 	
-	public void createJson(List<String> list){
+	public void createJson(ArrayList<String> list){
 		
 		ArrayList<Integer> end_points = new ArrayList<Integer>();
 		
 		int j = 0;
-		int num_common_idx = 0;
-		int last_common_idx = 0;
-		
+		int common_idx = 0;
 		for(String entry : list){
 			String [] components = entry.split("/");
 			int i;
 			
 			//Check if leaf node of this path already has been seen
-			num_common_idx = 0;
+			common_idx = 0;
 			JSONObject common = new JSONObject();
-			common.put("name", components[num_common_idx]);
+			common.put("name", components[common_idx]);
 			
 			while(nodes.contains(common)){
-				last_common_idx = nodes.indexOf(common);
 				common.clear();
 				
-				if(++num_common_idx < components.length){
-					String nodename = components[num_common_idx];
-					if(nodename.startsWith("DONOTLINK")){
-						nodename = nodename.replace("DONOTLINK", "LIST");
-					}
-					common.put("name", nodename);
-				}
+				if(++common_idx < components.length)
+					common.put("name", components[common_idx]);
 				else
 					break;
 			}
-			for(i = j; i-j+num_common_idx < components.length; i++){
+			
+			for(i = j; i-j+common_idx < components.length; i++){
 				JSONObject name = new JSONObject();
-				String nodename = components[i-j+num_common_idx];
+				String nodename = components[i-j+common_idx];
 				if(nodename.startsWith("DONOTLINK")){
 					nodename = nodename.replace("DONOTLINK", "LIST");
 				}
 				name.put("name", nodename);
 				nodes.add(name);
 				
-				if(i != j+components.length-1-num_common_idx){
+				if(i != j+components.length-1-common_idx){
 					JSONObject link = new JSONObject();
 					
-					if(i == j && num_common_idx > 0){
+					if(i == j && common_idx > 0){
 						JSONObject common_link = new JSONObject();
-						common_link.put("source", last_common_idx);
+						common_link.put("source", i-common_idx);
 						common_link.put("target", i);
 						links.add(common_link);
 						link.put("source", i);
@@ -77,10 +68,7 @@ public class GenerateOutputJson {
 					links.add(link);
 				}
 			}
-			
-			if(num_common_idx != components.length){
-				end_points.add(j+components.length-1-num_common_idx);
-			}
+			end_points.add(j+components.length-1-common_idx);
 			j = i;
 		}
 		
@@ -92,7 +80,7 @@ public class GenerateOutputJson {
 				links.add(link);
 			}
 		}
-//		System.out.println(links.toJSONString());
+		System.out.println(links.toJSONString());
 		JSONObject final_json = new JSONObject();
 		
 		final_json.put("nodes", nodes);
@@ -113,15 +101,12 @@ public class GenerateOutputJson {
 	}
 
 	public static void main(String[] args) {
-		String [] arr = {"yelp_academic_dataset_business_1.json/DONOTLINK_2/city/dravosburg", "yelp_academic_dataset_business_1.json/DONOTLINK_2", "yelp_academic_dataset_business_1.json/DONOTLINK_204/attributes", "yelp_academic_dataset_business_1.json/DONOTLINK_204/attributes/Good For", "yelp_academic_dataset_business_1.json/DONOTLINK_204/attributes/Good For/latenight"};
-
-		List<String> list = new ArrayList<String>();
-		list = Arrays.asList(arr);
-//		list.add("DOC1/b/tom");
-//		list.add("DOC1/b/e/tom");
-//		list.add("DOC2/d/tom");
-//		list.add("DOC3/tom");
-//		list.add("DOC4/x/brady");
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("DOC1/b/tom");
+		list.add("DOC1/b/e/tom");
+		list.add("DOC2/d/tom");
+		list.add("DOC3/tom");
+		list.add("DOC4/x/brady");
 		
 		GenerateOutputJson col = new GenerateOutputJson();
 		col.createJson(list);

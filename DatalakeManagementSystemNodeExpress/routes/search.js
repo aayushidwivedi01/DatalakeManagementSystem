1452daad13b4b04a102a0f3f3346a2204c81dcc0
aayushidwivedi01 <aayushi20@gmail.com>
2016-05-06 23:24:48 +0100

    integrating searchengine

--- DatalakeManagementSystemNodeExpress/routes/search.js
+++ DatalakeManagementSystemNodeExpress/routes/search.js
@@ -1,7 +1,7 @@
 var util = require("util");
 var java = require("java");
 java.classpath.push("google-collections-1.0-rc2.jar");
-java.classpath.push("searchengine.jar");
+java.classpath.push("search2.jar");
 var url = require('url');
 
 function searchResponse(err, data){
@@ -16,7 +16,8 @@ function search(req,res, err){
 			"searchengine.SearchEngine",
 			request.query, 
 			req.session.user);
-	java.callMethodSync(searchEngine, "search", searchResponse);
+	java.callMethodSync(searchEngine, "search", 
+			searchResponse);
 
 	res.render('results', {
 		result: request.query

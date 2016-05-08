1cf3c76e22ba23d84448686367742b8da23a73d8
aayushidwivedi01 <aayushi20@gmail.com>
2016-05-09 00:50:53 +0100

    Added graphs to search result

--- DatalakeManagementSystemNodeExpress/routes/graph.js
+++ DatalakeManagementSystemNodeExpress/routes/graph.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var url = require('url');
+var java = require('java');
 var router = express.Router();
+java.classpath.push("generateJSON.jar");
 
 function get_graph(req, res, next) {
 	var node = url.parse(req.url, true).query.node;
-	res.send(node);
+	var generateJSON = java.newInstanceSync(
+			"searchengine.GenerateOutputJson");
+	
+	var results = java.callMethodSync(generateJSON, "getData", node);
+	//var results = JSON.stringify(dataset);
+	var test = "{this is a sample}";
+	console.log("JSON: "+ results);
+	console.log("JSONString: "+ JSON.stringify(results));
+	res.render('graph',{
+		dataset:JSON.stringify(results)
+	});
 }
 
 exports.do_work = function(req, res, next){
